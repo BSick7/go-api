5d7573dc33b021acce5e77dc40bea54ffc6807cd
Brad Sickles <brad.sickles@gmail.com>
2021-02-04 09:41:34 -0500

    Panic middleware adds to error reporter

--- recovery/panic_middleware.go
+++ recovery/panic_middleware.go
@@ -1,15 +1,49 @@
 package recovery
 
 import (
+	"fmt"
 	"log"
+	"net/http"
 	"os"
+	"runtime/debug"
 
-	"github.com/gorilla/handlers"
+	"github.com/BSick7/go-api/errors"
 	"github.com/gorilla/mux"
 )
 
+type PanicError struct {
+	Err   interface{}
+	Stack []byte
+}
+
+func (e PanicError) Error() string {
+	return fmt.Sprintf("%s: %s", e.Err, e.Stack)
+}
+
 func PanicMiddleware() mux.MiddlewareFunc {
 	panicLogger := log.New(os.Stderr, "[PANIC] ", 0)
-	return handlers.RecoveryHandler(handlers.RecoveryLogger(panicLogger),
-		handlers.PrintRecoveryStack(true))
+	report := func(err interface{}, r *http.Request) {
+		perr := PanicError{
+			Err:   err,
+			Stack: debug.Stack(),
+		}
+		container := errors.ContainerFromContext(r.Context())
+		if container != nil {
+			container.AddError(perr)
+		}
+		panicLogger.Println(perr)
+	}
+
+	return func(handler http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			defer func() {
+				if err := recover(); err != nil {
+					w.WriteHeader(http.StatusInternalServerError)
+					report(err, r)
+				}
+			}()
+
+			handler.ServeHTTP(w, r)
+		})
+	}
 }
