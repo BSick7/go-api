85ad397f553462cd2e61c98f3f30f4e31d4b9f5f
Brad Sickles <brad.sickles@gmail.com>
2021-08-09 16:45:09 -0400

    Replace panic middleware with custom and added user-supplied fn

--- recovery/panic_middleware.go
+++ recovery/panic_middleware.go
@@ -2,14 +2,42 @@ package recovery
 
 import (
 	"log"
+	"net/http"
 	"os"
+	"runtime/debug"
 
-	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
-func PanicMiddleware() mux.MiddlewareFunc {
+type PanicRecoveryFunc func(req *http.Request, err interface{})
+
+func PanicMiddleware(fns ...PanicRecoveryFunc) mux.MiddlewareFunc {
 	panicLogger := log.New(os.Stderr, "[PANIC] ", 0)
-	return handlers.RecoveryHandler(handlers.RecoveryLogger(panicLogger),
-		handlers.PrintRecoveryStack(true))
+	return func(next http.Handler) http.Handler {
+		return &panicRecoveryHandler{
+			next: next,
+			fn: func(req *http.Request, err interface{}) {
+				panicLogger.Println(err)
+				debug.PrintStack()
+				for _, fn := range fns {
+					fn(req, err)
+				}
+			},
+		}
+	}
+}
+
+type panicRecoveryHandler struct {
+	next http.Handler
+	fn   PanicRecoveryFunc
+}
+
+func (h *panicRecoveryHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	defer func() {
+		if err := recover(); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			h.fn(req, err)
+		}
+	}()
+	h.next.ServeHTTP(w, req)
 }
