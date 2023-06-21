77a2aaae2156c9592b29bfcb06c5385f3d551e49
Brad Sickles <brad.sickles@gmail.com>
2023-06-21 11:56:41 -0400

    fmt

--- auth/authorize.go
+++ auth/authorize.go
@@ -12,7 +12,9 @@ type Authorizer func(r *http.Request) (*http.Request, error)
 
 // AuthorizeMiddleware creates an authorization middleware to respond based on the result of an authorization sequence
 // If using gorilla mux router, this middleware will only hit when a route is matched
-//   This is because gorilla matches a route, *then* runs the full chain of middlewares + handler
+//
+//	This is because gorilla matches a route, *then* runs the full chain of middlewares + handler
+//
 // Because of this, this makes mux.Vars(r) available to use to grab path parameters that gorilla parsed
 func AuthorizeMiddleware(authorize Authorizer) mux.MiddlewareFunc {
 	return func(next http.Handler) http.Handler {
