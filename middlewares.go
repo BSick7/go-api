f767d29ab58ec5694bb0282af01da3a692fe50e9
Brad Sickles <brad.sickles@gmail.com>
2024-03-01 20:46:16 -0500

    comments

--- middlewares.go
+++ middlewares.go
@@ -19,6 +19,9 @@ func (c *MiddlewareChain) Use(mwf ...mux.MiddlewareFunc) {
 	}
 }
 
+// Apply takes an input http.Handler and applies the middleware chain over a single http.Handler
+// These middlewares are chained in the order they are registered
+// If middleware A is registered earlier than B, then execution will be A => B => handler
 func (c *MiddlewareChain) Apply(handler http.Handler) http.Handler {
 	cur := handler
 	for i := len(c.Middlewares) - 1; i >= 0; i-- {
