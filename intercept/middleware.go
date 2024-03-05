867dd78e978e988cc2f1cde625c24307995c7edf
Brad Sickles <brad.sickles@gmail.com>
2024-03-04 20:47:48 -0500

    Use httpsnoop to create intercept Middleware

--- intercept/middleware.go
+++ intercept/middleware.go
@@ -1,78 +1,34 @@
 package intercept
 
 import (
-	"bufio"
-	"fmt"
-	"net"
-	"net/http"
-	"strings"
-	"time"
-
+	"github.com/felixge/httpsnoop"
 	"github.com/gorilla/mux"
+	"net/http"
+	"time"
 )
 
-type ResponseData interface {
-	StatusCode() int
-	Body() string
+type ResponseData struct {
+	// StatusCode represents the response's status code
+	StatusCode int
+	// Duration is the total duration of the request/response
+	Duration time.Duration
+	// Written is the number of bytes written in the response
+	Written int64
 }
 
-type OnResponseFunc func(r *http.Request, data ResponseData, duration time.Duration)
+type OnResponseFunc func(r *http.Request, data ResponseData)
 
-func Middleware(captureBody bool, onResponses ...OnResponseFunc) mux.MiddlewareFunc {
+func Middleware(onResponses ...OnResponseFunc) mux.MiddlewareFunc {
 	return func(handler http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			start := time.Now()
-			wrapped := &responseWriterInterceptor{ResponseWriter: w, captureBody: captureBody, statusCode: http.StatusOK}
-			handler.ServeHTTP(wrapped, r)
+			m := httpsnoop.CaptureMetrics(handler, w, r)
 			for _, onResponse := range onResponses {
-				onResponse(r, wrapped, time.Since(start))
+				onResponse(r, ResponseData{
+					StatusCode: m.Code,
+					Duration:   m.Duration,
+					Written:    m.Written,
+				})
 			}
 		})
 	}
-}
-
-var _ ResponseData = &responseWriterInterceptor{}
-var _ http.Hijacker = &responseWriterInterceptor{}
-
-type responseWriterInterceptor struct {
-	http.ResponseWriter
-	captureBody  bool
-	statusCode   int
-	capturedData []string
-}
-
-func (w *responseWriterInterceptor) Hijack() (net.Conn, *bufio.ReadWriter, error) {
-	if hijacker, ok := w.ResponseWriter.(http.Hijacker); ok {
-		return hijacker.Hijack()
-	}
-	return nil, nil, fmt.Errorf("can't switch protocols using non-Hijacker ResponseWriter type %T", w.ResponseWriter)
-}
-
-func (w *responseWriterInterceptor) StatusCode() int {
-	return w.statusCode
-}
-
-func (w *responseWriterInterceptor) Body() string {
-	if w.captureBody {
-		return strings.Join(w.capturedData, "")
-	}
-	return ""
-}
-
-func (w *responseWriterInterceptor) Header() http.Header {
-	return w.ResponseWriter.Header()
-}
-
-func (w *responseWriterInterceptor) Write(data []byte) (int, error) {
-	if w.captureBody {
-		if w.statusCode >= 400 {
-			w.capturedData = append(w.capturedData, string(data))
-		}
-	}
-	return w.ResponseWriter.Write(data)
-}
-
-func (w *responseWriterInterceptor) WriteHeader(statusCode int) {
-	w.statusCode = statusCode
-	w.ResponseWriter.WriteHeader(statusCode)
 }
